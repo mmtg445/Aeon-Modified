dea935b4e3ca0a6340a96d7fb41fccb2ef8a0450
Hrishikesh <hrishithombare15@gmail.com>
2024-07-08 16:08:07 +0530

    Blockquote

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -209,7 +209,7 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        msg += f"<b>{download.status()}:</b>"
+        msg += f"<b>{download.status()}:</b>\n"
         msg += f"<b>\n⌑ ғɪʟᴇɴᴀᴍᴇ</b> » <i>{escape(f'{download.name()}')}</i>\n"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n⌑ {progress_bar(download.progress())} » {download.progress()}"
